27515fa70c2b842e6455fd0254c8d6267f934df0
MikeAdu <mike_adusah@yahoo.com>
2021-08-04 16:34:50 -0700

    adding routes

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const store = require("../db/store");
+
+// get request should return notes from DB
+router.get("/api/notes", (req, res) => {
+
+ })
+
+ //post request should add notes to db 
+ router.post("/api/notes", (req, res) => {
+    
+})
